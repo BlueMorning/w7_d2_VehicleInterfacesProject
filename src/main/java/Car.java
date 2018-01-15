ba4a88cd955f2761cd2c09fd56ac20f7690ac2ae
Eric Carles <carles.eric@wanadoo.fr>
2018-01-15 22:06:29 +0000

    Class car added

---
+++ src/main/java/Car.java
@@ -0,0 +1,6 @@
+public class Car extends Vehicle {
+
+    public Car(String name, int minSpeed, int maxSpeed) {
+        super(name, minSpeed, maxSpeed);
+    }
+}
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CarTest {
+
+    Car car;
+
+    @Before
+    public void Before(){
+        car = new Car("City", 5, 120);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("City", car.getName());
+    }
+
+    public void hasMinSpeed(){
+        assertEquals(5, car.getMaxSpeed());
+    }
+
+    public void hasMaxSpeed(){
+        assertEquals(120, car.getMaxSpeed());
+    }
+
+
 }
